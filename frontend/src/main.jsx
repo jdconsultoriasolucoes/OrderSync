daf87164f223a6ed36cb3b229200157b4d75788a
JD Consultoria Soluções <jdconsultoriasolucoes@gmail.com>
2025-12-14 16:36:41 -0300

    ajuste retirada cache

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
-import '../styles.css'
-import '../navbar.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
